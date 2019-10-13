17e10c7097ea0589604e145fab441a44eec9a4d5
Dwight Marie <dwightmarie@gmail.com>
2019-10-13 02:29:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,27 +17,25 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-array = [1,2,3,4,5,6]
- array.pop
-end
-
-remove_element_from_end_of_array(array)
 array = []
 array.shift
  end
 
 def remove_element_from_start_of_array(array)
-  
+  array = []
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = []
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return array.shift
 end
 
 def retrieve_last_element_from_array(array)
+  array = []
   
 end
