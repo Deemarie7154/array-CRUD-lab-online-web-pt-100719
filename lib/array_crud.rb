29dcfe7d9b02a6fc170c42d2acf74d0667ee2d75
Dwight Marie <dwightmarie@gmail.com>
2019-10-13 01:51:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,22 +1,26 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  currency = ["bitcoin", "gold", "euro", "cash"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
-end
+  array = []
+  array.pop
+
+ end
 
 def remove_element_from_start_of_array(array)
   
